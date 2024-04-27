e23e765cb8a7bba77ff911a3a364e3d425201b4a
Zhakya Nuristany <89075691+nuristany@users.noreply.github.com>
2024-04-27 22:27:05 +0430

    commented the settings for email to troubleshoot the error

--- limousin_services/settings_rail.py
+++ limousin_services/settings_rail.py
@@ -9,8 +9,8 @@ SECRET_KEY = config('SECRET_KEY')
 
 
 # Code that relies on the configuration variables
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT', cast=int)
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
+# EMAIL_HOST = config('EMAIL_HOST')
+# EMAIL_PORT = config('EMAIL_PORT', cast=int)
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_USE_TLS = config('EMAIL_USE_TLS', cast=bool)
